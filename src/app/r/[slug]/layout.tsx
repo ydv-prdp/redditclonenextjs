1bc837e2d8e50478e9a9502c7fc89b6da1627dbc
Pradeep Yadav <pradeepyadav101010@gmail.com>
2023-06-18 13:38:51 +0530

    post creation functionality added with image upload and editor

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,7 +1,9 @@
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle"
+import { buttonVariants } from "@/components/ui/Button"
 import { getAuthSession } from "@/lib/auth"
 import { db } from "@/lib/db"
 import { format } from "date-fns"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 
 const Layout = async ({children, params: {slug}, }: {
@@ -87,6 +89,11 @@ const memberCount = await db.subscription.count({
                                  isSubscribed={isSubscribed}
                                  />
                             ):null}
+
+                            <Link href={`r/${slug}/submit`} className={buttonVariants({
+                                variant:'outline',
+                                className:'w-full mb-6'
+                            })} >Create Post</Link>
 
                         </dl>
 
